9542768bd192d3016063687ecf480c67068ad793
Rohit Verma <rv17837@outlook.com>
2023-11-24 14:55:06 +0530

    Updated Configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
-import { defineConfig } from "rollup";
+import { defineConfig } from "vite";
 
-export default defineConfig({
-    base: "/Test-Deployment"
+export default defineConfig(({ command, mode, isSsrBuild })=> {
+    console.log(command, mode, isSsrBuild)
+    if(mode === 'production') {
+        return {
+            base: "/Test-Deployment/"
+        }
+    }
+
+    return {}
 })
